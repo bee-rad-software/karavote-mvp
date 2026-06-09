0c3809ba6ee91ff13a24fb2940e679dff58b502a
bee-rad-software <bbock999@gmail.com>
2026-06-09 11:43:23 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'KaraVote',
+  title: 'StageVotes',
   description: 'Karaoke contest voting app'
 };
 
