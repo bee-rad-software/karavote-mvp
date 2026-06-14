0b349c4eee4587ea3c2679930caad3ef6502ee59
bee-rad-software <bbock999@gmail.com>
2026-06-14 14:35:32 -0500

    Update page.tsx

--- app/host/[eventId]/page.tsx
+++ app/host/[eventId]/page.tsx
@@ -1172,19 +1172,41 @@ const singerGroups = activeQueue.reduce((groups, p) => {
   <hr style={{ margin: '24px 0', opacity: 0.15 }} />
 
   <label>
-    <input type="checkbox" checked={showSignupQR} onChange={() => toggleQrSetting('show_signup_qr')} />
-    Show Signup QR
-  </label>
+  <input
+    type="checkbox"
+    checked={!!event?.show_signup_qr}
+    onChange={(e) =>
+      toggleQrSetting('show_signup_qr', e.target.checked)
+    }
+  />
+  Show Signup QR
+</label>
 
-  <label>
-    <input type="checkbox" checked={showVotingQR} onChange={() => toggleQrSetting('show_voting_qr')} />
-    Show Voting QR
-  </label>
+<label>
+  <input
+    type="checkbox"
+    checked={!!event?.show_voting_qr}
+    onChange={(e) =>
+      toggleQrSetting('show_voting_qr', e.target.checked)
+    }
+  />
+  Show Voting QR
+</label>
 
-  <label>
-    <input type="checkbox" checked={showPeoplesChoiceQR} onChange={() => toggleQrSetting('show_peoples_choice_qr')} />
-    Show People's Choice QR
-  </label>
+<label>
+  <input
+    type="checkbox"
+    checked={!!event?.show_peoples_choice_qr}
+    onChange={(e) =>
+      toggleQrSetting(
+        'show_peoples_choice_qr',
+        e.target.checked
+      )
+    }
+  />
+  Show People's Choice QR
+</label>
+  
 </div>
 <div className="card">
   <h2>📍 Check-In Settings</h2>
